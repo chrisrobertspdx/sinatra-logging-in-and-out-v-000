7b189d20e17a6dbf6d8472a8d5a41030c7f4f992
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-22 02:30:38 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,8 +1,8 @@
 class Helpers
-  def is_logged_in?(info)
+  def self.is_logged_in?(info)
     !!info[:user_id]
   end
-  def current_user(info)
+  def self.current_user(info)
     @user = User.find(info[:user_id])
   end
 end
